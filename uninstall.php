4c4a0412ff2c2da7231f5ceee95b742408a7c3ce
roverchen <rover.k.chen@gmail.com>
2021-10-28 16:26:16 +0800

    /* $option_name = 'wporg_option';
    delete_option($option_name);
    
    // for site options in Multisite
    delete_site_option($option_name);
    */

--- uninstall.php
+++ uninstall.php
@@ -3,19 +3,16 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
-global $wpdb;
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}member_badges" );
+
 /*
 $option_name = 'wporg_option';
  
 delete_option($option_name);
  
 // for site options in Multisite
-delete_site_option($option_name);
- 
+delete_site_option($option_name); 
+*/
 // drop a custom database table
 global $wpdb;
-//$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mytable");
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}courses" );
-*/
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}member_badges" );
 ?>
